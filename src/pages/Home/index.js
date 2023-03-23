edb7622e79570508241fe8f302c3ad79197f58cd
Gustavo-Cruz-Pinheiro <54116994+Gustavo-Cruz-Pinheiro@users.noreply.github.com>
2023-03-23 09:12:45 -0400

    Add files via upload

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -6,9 +6,50 @@ import "../../App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function Home() {
+    const sair = () => {
+        localStorage.clear();
+        window.location.reload();
+      }
+
     return(
         <div>
-            <Nav/>
+            <nav className="navbar navbar-expand navbar-dark bg-success py-3 flex-column flex-md-row">
+                <Link to={"/produtos"} className="navbar-brand">
+                    <img src={logo} width="30" height="30" className="d-inline-block align-top" alt="Logo"></img>
+                    GCP E-commerce
+                </Link>
+                <div className="navbar-nav-scroll">
+                    <ul className="navbar-nav bd-navbar-nav flex-row">
+                        <li className="nav-item">
+                            <Link to={"/"} className="nav-link">
+                                Home
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to={"/produtos"} className="nav-link">
+                                Gerenciar Produtos
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to={"/adicionar_produto"} className="nav-link">
+                                Adicionar Produto
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to={`/usuarios/${usuario.id}`} className="nav-link">
+                                Editar Perfil
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <div
+                                onClick={sair}
+                                className="nav-link" style={{cursor: "pointer"}}>
+                                    Sair
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
             <main className="container">
                 <Produtos/>
             </main>
